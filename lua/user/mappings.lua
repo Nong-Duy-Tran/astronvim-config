43ce080aa3e11cf21b305e73cdc5640274457d21
Ethan <geekologitech@gmail.com>
2025-12-21 15:28:11 +0700

    feat: add buffer navigation

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -2,3 +2,5 @@
 vim.keymap.set("n", "<C-\\>", '<Cmd>execute v:count . "ToggleTerm"<CR>', { desc = "Toggle terminal" })
 vim.keymap.set("i", "<C-\\>", '<Cmd>execute v:count . "ToggleTerm"<CR>', { desc = "Toggle terminal" })
 vim.keymap.set("t", "<C-\\>", '<Cmd>execute v:count . "ToggleTerm"<CR>', { desc = "Toggle terminal" })
+vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer →" })
+vim.keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Prev buffer ←" })
